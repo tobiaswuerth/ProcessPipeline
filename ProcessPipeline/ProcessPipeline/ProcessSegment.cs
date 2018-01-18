f210bf6a7fbb100acdf9c0406970bc41c11e3f7b
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2018-01-18 10:15:12 +0100

    create process segment

---
+++ ProcessPipeline/ProcessPipeline/ProcessSegment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ch.wuerth.tobias.ProcessPipeline
+{
+    public abstract class ProcessSegment<TFrom, TTo> : IProcessReceiver<TFrom>
+    {
+        public IProcessReceiver<TTo> Next { get; private set; }
+
+        public void Receive(TFrom obj)
+        {
+            TTo processed = OnProcess(obj);
+            Next?.Receive(processed);
+        }
+
+        public void Append(IProcessReceiver<TTo> next)
+        {
+            Next = next ?? throw new ArgumentNullException(nameof(next));
+        }
+
+        protected abstract TTo OnProcess(TFrom obj);
+    }
+}
